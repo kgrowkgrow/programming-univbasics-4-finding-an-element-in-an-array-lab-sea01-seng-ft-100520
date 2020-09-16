ae3168084363e3ced9c975eece627f7b14b62e2e
Kevin Grow <kevingrow11@gmail.com>
2020-09-16 20:00:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   counter = 0 
-  found_value_index = nil
+  found_value_index = nil 
   while counter < array.length do 
     if array[counter] == value_to_find
       found_value_index = counter
