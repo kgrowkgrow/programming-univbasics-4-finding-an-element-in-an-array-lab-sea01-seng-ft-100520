ef6444bb37aa58594b811afc8d221d8bac6bb983
Kevin Grow <kevingrow11@gmail.com>
2020-09-16 20:04:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,8 @@
 def find_element_index(array, value_to_find)
-  
+  count = 0 
+  while count < value_to_find do 
+    if value_to_find == array[count]
+      return array[count]
+    end 
+    count += 1
 end
